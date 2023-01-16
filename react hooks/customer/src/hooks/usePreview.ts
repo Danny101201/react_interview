d979ce43bd764d1f189e4697b9fd2702ae7edd8b
Danny <hiunji64@gmail.com>
2023-01-16 11:29:13 +0800

    feat: add customer hooks

---
+++ react hooks/customer/src/hooks/usePreview.ts
@@ -0,0 +1,18 @@
+import { useRef, useEffect } from 'react'
+export const usePreview = <T>(value: T) => {
+  const currentValue = useRef<T>(value)
+  const prevValue = useRef<T>()
+  useEffect(() => {
+    if (currentValue.current !== value) {
+      prevValue.current = currentValue.current
+      currentValue.current = value
+    }
+  }, [value])
+  return {
+    current: currentValue.current,
+    preView: prevValue.current
+  } as {
+    current: T,
+    preView: T,
+  }
+}
