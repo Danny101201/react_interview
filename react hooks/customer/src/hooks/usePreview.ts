cfa2ced423c11d58b9f6955b68b45dbf29c40a30
Danny <hiunji64@gmail.com>
2023-01-16 11:44:17 +0800

    fix: error page

--- react hooks/customer/src/hooks/usePreview.ts
+++ react hooks/customer/src/hooks/usePreview.ts
@@ -2,12 +2,10 @@ import { useRef, useEffect } from 'react'
 export const usePreview = <T>(value: T) => {
   const currentValue = useRef<T>(value)
   const prevValue = useRef<T>()
-  useEffect(() => {
-    if (currentValue.current !== value) {
-      prevValue.current = currentValue.current
-      currentValue.current = value
-    }
-  }, [value])
+  if (currentValue.current !== value) {
+    prevValue.current = currentValue.current
+    currentValue.current = value
+  }
   return {
     current: currentValue.current,
     preView: prevValue.current
